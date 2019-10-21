d021a93da2669992129e8e7db6b76256c9427a86
Maxence Valette <maxence.valette@hotmail.fr>
2019-10-21 22:11:32 +0200

    initial commit

---
+++ lib/gossip.rb
@@ -0,0 +1,40 @@
+require 'pry'
+require 'csv'
+class Gossip
+	attr_accessor :author, :content
+
+	def initialize(author, content)
+		@author = author
+		@content = content
+	end
+
+	def save
+		CSV.open("./db/gossip.csv", "ab") do |csv|
+	    csv << [@author, @content]
+	  end
+	end
+
+	def self.find(id)
+		all_gossips = Gossip.all
+		return all_gossips[id.to_i]
+	end
+
+	def self.all
+	  all_gossips = []
+	  CSV.read("./db/gossip.csv").each do |csv_line|
+	    all_gossips << Gossip.new(csv_line[0], csv_line[1])
+	  end
+	  return all_gossips
+	end
+
+	def self.update(author, content, id)
+		gossip_array = self.all
+		gossip_array[id.to_i].content = content
+		gossip_array[id.to_i].author = author
+		File.open("./db/gossip.csv", 'w') {|file| file.truncate(0) }
+		gossip_array.each do |gossip|
+			gossip.save
+		end
+	end
+
+end
